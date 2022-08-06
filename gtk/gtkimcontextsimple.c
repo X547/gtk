31a80bddc7e51f244159ba4d72c9e09fed86c33a
Luke Horwell <code@horwell.me>
2022-08-07 02:28:08 +0900

    other__remove_dead_keys_underline
    ===================================================================

--- gtk/gtkimcontextsimple.c
+++ gtk/gtkimcontextsimple.c
@@ -1133,7 +1133,7 @@ gtk_im_context_simple_get_preedit_string (GtkIMContext   *context,
                   gunichar ch;
                   gboolean need_space;
 
-                  if (is_dead_key (priv->compose_buffer[i]))
+                  if (FALSE && is_dead_key (priv->compose_buffer[i]))
                     {
                       ch = dead_key_to_unicode (priv->compose_buffer[i], &need_space);
                       if (ch)
